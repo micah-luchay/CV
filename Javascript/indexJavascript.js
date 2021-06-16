1610f693a3bf15f09a003eb6b5adb670d8cad2eb
Micah <85366139+micah-luchay@users.noreply.github.com>
2021-06-16 19:26:46 -0400

    Add files via upload

--- Javascript/indexJavascript.js
+++ Javascript/indexJavascript.js
@@ -8,11 +8,13 @@ class slideShowSlide {  // class for slide show objects
 }
 
 let Irmo = new slideShowSlide(0, "Map detailing sewer assets in the Lexington area.", "Photos\\Examples\\Slideshow\\Town of Irmo Meeting.png", "City owned assets in Lexington area");
-let CIU = new slideShowSlide(1, "Presentation map to inform university officials of City owned utilities near CIU.", "Photos\\Examples\\Slideshow\\Columbia International University Infrastructure 24x36 V2.png", "Utilities near CIU");
-let YCHA = new slideShowSlide(2, "Deliverable to Yacht Cove Homeowners Association regarding City vs. Privately owned utility assets.", "Photos\\Examples\\Slideshow\\Yacht Cove_Reduced.png", "Private vs. City owned assets at Yacht Cove");
+let FLOW = new slideShowSlide(1, "Deliverable depicting the upstream basin of a flow meter to isolate I&I.", "Photos\\Examples\\Slideshow\\Meter SR02A Basin.png", "Upstream basin of a flow meter");
+let CIU = new slideShowSlide(2, "Presentation map to inform university officials of City owned utilities near CIU.", "Photos\\Examples\\Slideshow\\Columbia International University Infrastructure 24x36 V2.png", "Utilities near CIU");
+let YCHA = new slideShowSlide(3, "Deliverable to Yacht Cove Homeowners Association regarding City vs. Privately owned utility assets.", "Photos\\Examples\\Slideshow\\Yacht Cove_Reduced.png", "Private vs. City owned assets at Yacht Cove")
 
 slideShowData = []; // array to hold the objects
 slideShowData.push(Irmo); // add the objects to the arrary
+slideShowData.push(FLOW);
 slideShowData.push(CIU);
 slideShowData.push(YCHA);
 
