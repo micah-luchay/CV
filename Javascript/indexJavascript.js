5c7ed2db5808b9a4e949da2bf4b1135ff297c506
Micah <85366139+micah-luchay@users.noreply.github.com>
2026-05-03 15:40:22 -0400

    Update indexJavascript.js

--- Javascript/indexJavascript.js
+++ Javascript/indexJavascript.js
@@ -1,4 +1,8 @@
 var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
 class slideShowSlide {  // class for slide show objects
 	constructor(index, description, photoSrc, photoAlt) {
