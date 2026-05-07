b84754a571500f82960ef4e0f26d2c8cfc430bc4
Micah <85366139+micah-luchay@users.noreply.github.com>
2026-05-06 21:13:12 -0400

    Update indexJavascript.js

--- Javascript/indexJavascript.js
+++ Javascript/indexJavascript.js
@@ -7,7 +7,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 fetch("https://9mwhsiw89d.execute-api.us-east-2.amazonaws.com/retrieve-geojson")
   .then(response => response.json())
   .then(data => {
-    L.geoJSON(data).addTo(map);
+    weatherData = L.geoJSON(data).addTo(map);
+	weatherData.popup();
   })
   .catch(error => console.error(error));
 
