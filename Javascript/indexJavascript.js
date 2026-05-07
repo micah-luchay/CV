412553ea37e828c8ccca23f2e1167636f49beb46
Micah <85366139+micah-luchay@users.noreply.github.com>
2026-05-06 21:52:42 -0400

    Update indexJavascript.js

--- Javascript/indexJavascript.js
+++ Javascript/indexJavascript.js
@@ -6,7 +6,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 function onEachFeature(feature, layer) {
     // does this feature have a property named popupContent?
-    if (feature.properties && feature.properties.popupContent) {
+    if (feature.properties && feature.properties.longitude) {
         layer.bindPopup(feature.properties.longitude);
     }
 }
