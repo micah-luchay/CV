eb5aa10308dccdd35f83856b42e569be499d5d02
Micah <85366139+micah-luchay@users.noreply.github.com>
2026-05-12 18:22:21 -0400

    Update indexJavascript.js

--- Javascript/indexJavascript.js
+++ Javascript/indexJavascript.js
@@ -15,7 +15,7 @@ function onEachFeature(feature, layer) {
 	let popupContent = "";
 	
     for (const key in feature.properties) {
-		var column = ${key}.charAt(0).toUpperCase() + ${key}.slice(1).toLowerCase();
+		var column = `${key}.charAt(0).toUpperCase() + ${key}.slice(1).toLowerCase()`;
         popupContent += `<b>${column}</b>: ${feature.properties[key]}${popupUnits[key]} ?? ""<br>`;
     }
         layer.bindPopup(popupContent).openPopup();
