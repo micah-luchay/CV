1de6cbccc07683f51591edeb2b3c00f63866ef48
Micah <85366139+micah-luchay@users.noreply.github.com>
2026-05-12 21:05:38 -0400

    Update indexJavascript.js

--- Javascript/indexJavascript.js
+++ Javascript/indexJavascript.js
@@ -20,7 +20,7 @@ function onEachFeature(feature, layer) {
         layer.bindPopup(popupContent).openPopup();
 }
 
-var iconSymbol = L.icon({
+var iconSymbol = L.Icon({
 	iconUrl: 'Photos/Icon/red-map-pin-3d-render-classic-location-marker_986584-61552.png'
 });
 
