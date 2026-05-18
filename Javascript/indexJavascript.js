12c12c94f4c959a4a5e9b72363fc4225d18e7276
Micah <85366139+micah-luchay@users.noreply.github.com>
2026-05-18 12:44:54 -0400

    Update indexJavascript.js

--- Javascript/indexJavascript.js
+++ Javascript/indexJavascript.js
@@ -21,7 +21,8 @@ function onEachFeature(feature, layer) {
 }
 
 var iconSymbol = L.icon({
-	iconUrl: 'Photos/Icon/red-map-pin-3d-render-classic-location-marker_986584-61552.png'
+	iconUrl: 'Photos/Icon/red-map-pin-3d-render-classic-location-marker_986584-61552.png',
+	iconSize: [2, 95]
 });
 
 var iconOptions = {
