aad308e181cb456c8ef67886a04d65c899ec9676
Micah <85366139+micah-luchay@users.noreply.github.com>
2026-05-18 13:23:28 -0400

    Update indexJavascript.js

--- Javascript/indexJavascript.js
+++ Javascript/indexJavascript.js
@@ -22,7 +22,7 @@ function onEachFeature(feature, layer) {
 
 var iconSymbol = L.icon({
 	iconUrl: 'Photos/Icon/red marker 1.png',
-	iconSize: [50, 50]
+	iconSize: [30, 30]
 });
 
 var iconOptions = {
